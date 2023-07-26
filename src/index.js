1350c7e2d508c3c4cf47e964b0fdd621dee63f32
aaronworsnop <aaronworsnop@gmail.com>
2023-07-26 18:42:55 +1200

    Initialised

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const port = 8000;
+const connectDb = require('./mongoDb/db');
+const notesRoutes = require('./routes/notes')
+
+connectDb();
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+app.use(express.json())
+app.use('/notes', notesRoutes);
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
